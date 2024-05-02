051b15c93b65b870b0fdab89762611f99cd36314
Brayan Mauricio-Gonzalez <bomauriciogonzalez1@catamount.wcu.edu>
2024-05-02 00:02:08 -0400

    Added docker

--- src/go.mod
+++ src/go.mod
@@ -1,6 +1,6 @@
 module qooked
 
-go 1.19
+go 1.22.1
 
 require github.com/gin-gonic/gin v1.9.1
 
--- src/main.go
+++ src/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -68,14 +67,5 @@ func main() {
 
     router.GET("/recipes", getRecipes)
 
-    // Azure App Service sets the port as an Environment Variable
-	// This can be random, so needs to be loaded at startup
-	port := os.Getenv("HTTP_PLATFORM_PORT")
-
-	// default back to 8080 for local development
-	if port == "" {
-		port = "8080"
-	}
-
-	router.Run("127.0.0.1:" + port)
+	router.Run()
 }
