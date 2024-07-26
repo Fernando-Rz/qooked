76f447c2919eef3f6cf3440f86c102f6dc91224b
Brayan Mauricio-Gonzalez <bomauriciogonzalez1@catamount.wcu.edu>
2024-07-26 01:06:52 -0400

    Added template for server class

---
+++ internal/http/server/server.go
@@ -0,0 +1,12 @@
+package server
+
+// Server definition
+type Server struct {
+	// configuration class
+	// instrumentation class
+	// router class
+}
+
+// initialize method
+
+// run server method
